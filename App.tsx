09933b25f5f49501675168fbb3ac888f1fa7a041
Pablo <martinez.slomp@gmail.com>
2022-09-19 21:52:05 -0300

    Navigate

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
 import { StatusBar, SafeAreaView, StyleSheet, View } from 'react-native';
 import Box from './scr/screens/Box';
 import { useFonts, Nunito_400Regular, Nunito_700Bold ,Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
-import LMedicines from './scr/components/LMedicine';
-import mock from './scr/mocks/medicines'
-import Body from './scr/screens/Box/components/Body';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from '@react-navigation/native';
+import Medicine from './scr/screens/AddMedicine/index';
+
+const Stack = createNativeStackNavigator();
+
+export enum ScreenNames {
+  Box = "Box",
+  Medicine = "Medicine",
+}
+
+ScreenNames.Box;
 
 export default function App() {
   const [font] = useFonts({
@@ -20,7 +29,15 @@ export default function App() {
     <SafeAreaView>
       
       <StatusBar />
-      <Box mock={mock}/>
+
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={ScreenNames.Box} component={Box}/>
+        <Stack.Screen name={ScreenNames.Medicine} component={Medicine}/>
+      </Stack.Navigator>
+      </NavigationContainer>
+
+      {/* <Box mock={mock}/> */}
       
     </SafeAreaView>
   );
