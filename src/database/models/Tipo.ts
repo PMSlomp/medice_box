ead8fbaa907ad0a97b9b31a27ffa3e31f4dfcd27
Andres <andres.porfirio@gmail.com>
2022-11-07 14:45:27 -0300

    migrated from typeorm to expo-sqlite-orm

---
+++ src/database/models/Tipo.ts
@@ -0,0 +1,23 @@
+import * as SQLite from "expo-sqlite";
+import { BaseModel, types } from "expo-sqlite-orm";
+
+export default class Tipo extends BaseModel {
+  constructor(obj: any) {
+    super(obj);
+  }
+
+  static get database() {
+    return async () => SQLite.openDatabase("database.db");
+  }
+
+  static get tableName() {
+    return "tipos";
+  }
+
+  static get columnMapping() {
+    return {
+      id: { type: types.INTEGER, primary_key: true, autoincrement: true }, // For while only supports id as primary key
+      tipo: { type: types.TEXT, not_null: true },
+    };
+  }
+}
